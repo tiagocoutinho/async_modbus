8fd0726a460527f784c4dcc8d4af47c6f7e1dd3e
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2022-01-21 06:25:05 +0100

    Bump version: 0.1.1 → 0.1.2

--- async_modbus.py
+++ async_modbus.py
@@ -21,7 +21,7 @@ import numpy
 
 __author__ = """Tiago Coutinho"""
 __email__ = "coutinhotiago@gmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 async def send_message_tcp(adu, reader, writer):
