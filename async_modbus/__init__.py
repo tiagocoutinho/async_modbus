ac97e7cc49bc186a7ac4fe756fe29869aaf5ab7c
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2022-02-19 06:24:36 +0000

    Add missing __init__.py

---
+++ async_modbus/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Async ModBus project
+#
+# Copyright (c) 2020-2022 Tiago Coutinho
+# Distributed under the GNU General Public License v3. See LICENSE for info.
+
+from .core import *
+
+__author__ = """Tiago Coutinho"""
+__email__ = "coutinhotiago@gmail.com"
+__version__ = "0.1.4"
+
